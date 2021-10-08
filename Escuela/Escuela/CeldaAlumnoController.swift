6f7ac249321f41fe6010454bb870a3ff5044e3ae
Marianne <53828111+macoam@users.noreply.github.com>
2021-10-07 17:25:19 -0700

    Add files via upload

---
+++ Escuela/Escuela/CeldaAlumnoController.swift
@@ -0,0 +1,9 @@
+//
+//  CeldaAlumnoController.swift
+//  Escuela
+//
+//  Created by Alumno on 10/7/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
---
+++ Escuela/Escuela/ListaAlumnosController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Escuela
+//
+//  Created by Alumno on 10/7/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ListaAlumnosController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
